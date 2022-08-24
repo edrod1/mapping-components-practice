aeeb07f59e0f4da44626d4264cd61352483f05da
Edgar <edgarx28@outlook.com>
2022-08-23 23:05:38 -0400

    Initial Commit

---
+++ src/emojipedia.js
@@ -0,0 +1,43 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+  {
+    id: 4,
+    emoji: "🤓",
+    name: "Nerd Face",
+    meaning: "Huge glasses, awkward smile and buck teeth. Used humorously or ironically for nerds or to express how smart you are. Stereotype of a nerd; a smart but funny-dressed person with social deficits."
+  },
+  {
+    id: 5,
+    emoji: "😎",
+    name: "Face With Sunglasses",
+    meaning: "I'm the king of the world! Mr. Cool Smiley with sunglasses. Is totally self-assured and relaxed. Everything is absolutely easy and cool. Also a symbol for sun, summer holidays, and holidays on the beach."
+  },
+  {
+    id: 6,
+    emoji: "🤌",
+    name: "Pinched fingers",
+    meaning: "What do you want?! “Ma che vuoi” is one of the most famous gestures in Italy. Universally applicable when someone asks for something or when you wish for something yourself. The more often the gesture is used, the more impatient or frustrated the other person is."
+  }
+];
+
+export default emojipedia;
